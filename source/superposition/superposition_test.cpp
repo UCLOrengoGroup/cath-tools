7d868bd2cbb4e16aded7e0365b9ceb5114bf94a4
Tony Lewis <tonyelewis@users.noreply.github.com>
2015-08-17 08:31:57 +0100

    Rename variables (trivial tweak)

--- source/superposition/superposition_test.cpp
+++ source/superposition/superposition_test.cpp
@@ -49,7 +49,7 @@ namespace cath {
 			~superposition_test_suite_fixture() noexcept = default;
 
 		public:
-			const coord_list coord_list1 { coord_vec{
+			const coord_list coord_list_1 { coord_vec{
 				{ 10.306, 134.301, 48.038 },
 				{ 13.342, 136.603, 47.735 },
 				{ 16.498, 135.180, 46.225 },
@@ -57,7 +57,7 @@ namespace cath {
 				{ 21.726, 135.373, 42.223 }
 			} };
 
-			const coord_list coord_list2 { coord_vec{
+			const coord_list coord_list_2 { coord_vec{
 				{ 21.222, 18.395, 14.116 },
 				{ 24.152, 16.183, 15.172 },
 				{ 23.817, 12.426, 14.864 },
@@ -65,7 +65,7 @@ namespace cath {
 				{ 25.362,  6.469, 12.836 }
 			} };
 
-			const coord_list coord_list2_variation { coord_vec{
+			const coord_list coord_list_2_variation { coord_vec{
 				{ 21.222, 18.395, 14.116 },
 				{ 24.152, 16.183, 15.172 },
 				{ 23.817, 12.426, 14.864 },
@@ -83,8 +83,8 @@ BOOST_FIXTURE_TEST_SUITE(superposition_test_suite, cath::test::superposition_tes
 /// \brief TODOCUMENT
 BOOST_AUTO_TEST_CASE(equality) {
 	const auto superpositions = {
-		create_pairwise_superposition( coord_list1, coord_list2           ),
-		create_pairwise_superposition( coord_list1, coord_list2_variation )
+		create_pairwise_superposition( coord_list_1, coord_list_2           ),
+		create_pairwise_superposition( coord_list_1, coord_list_2_variation )
 	};
 	check_equality_operators_on_diff_vals_range( superpositions );
 }
@@ -94,16 +94,16 @@ BOOST_AUTO_TEST_CASE(equality) {
 ///         - a translation to be applied to the base structure
 ///         - a rotation    to be applied to the base structure
 BOOST_AUTO_TEST_CASE(vary_base_rot_and_trans) {
-	const superposition test_sup_first         = create_pairwise_superposition( coord_list1, coord_list2, true  );
+	const superposition test_sup_first         = create_pairwise_superposition( coord_list_1, coord_list_2, true  );
 	const coord         sup_first_translation  = test_sup_first.get_translation_of_index(  superposition::INDEX_OF_SECOND_IN_PAIRWISE_SUPERPOSITION );
 	const rotation      sup_first_rotation     = test_sup_first.get_rotation_of_index(     superposition::INDEX_OF_SECOND_IN_PAIRWISE_SUPERPOSITION );
 
-	const superposition test_sup_second        = create_pairwise_superposition( coord_list1, coord_list2, false );
+	const superposition test_sup_second        = create_pairwise_superposition( coord_list_1, coord_list_2, false );
 	const coord         sup_second_translation = test_sup_second.get_translation_of_index( superposition::INDEX_OF_FIRST_IN_PAIRWISE_SUPERPOSITION  );
 	const rotation      sup_second_rotation    = test_sup_second.get_rotation_of_index(    superposition::INDEX_OF_FIRST_IN_PAIRWISE_SUPERPOSITION  );
 
-	const superposition test_sup_first_rev     = create_pairwise_superposition( coord_list1, coord_list2, true,  sup_second_translation, sup_second_rotation );
-	const superposition test_sup_second_rev    = create_pairwise_superposition( coord_list1, coord_list2, false, sup_first_translation,  sup_first_rotation  );
+	const superposition test_sup_first_rev     = create_pairwise_superposition( coord_list_1, coord_list_2, true,  sup_second_translation, sup_second_rotation );
+	const superposition test_sup_second_rev    = create_pairwise_superposition( coord_list_1, coord_list_2, false, sup_first_translation,  sup_first_rotation  );
 
 	BOOST_CHECK(  are_close( test_sup_first,  test_sup_second_rev ) );
 	BOOST_CHECK(  are_close( test_sup_second, test_sup_first_rev  ) );
@@ -114,21 +114,21 @@ BOOST_AUTO_TEST_CASE(vary_base_rot_and_trans) {
 ///
 /// \todo Have this testcase generate the flipped versions rather than hard-coding them in here
 BOOST_AUTO_TEST_CASE(axis_pair_negation_y_and_z) {
-	const coord_list coord_list1_flipped_around_x{ {
+	const coord_list coord_list_1_flipped_around_x{ {
 		{ 10.306, -134.301, -48.038 },
 		{ 13.342, -136.603, -47.735 },
 		{ 16.498, -135.180, -46.225 },
 		{ 19.855, -136.695, -45.235 },
 		{ 21.726, -135.373, -42.223 }
 	} };
-	const coord_list coord_list1_flipped_around_y{ {
+	const coord_list coord_list_1_flipped_around_y{ {
 		{ -10.306,  134.301, -48.038 },
 		{ -13.342,  136.603, -47.735 },
 		{ -16.498,  135.180, -46.225 },
 		{ -19.855,  136.695, -45.235 },
 		{ -21.726,  135.373, -42.223 }
 	} };
-	const coord_list coord_list1_flipped_around_z{ {
+	const coord_list coord_list_1_flipped_around_z{ {
 		{ -10.306, -134.301,  48.038 },
 		{ -13.342, -136.603,  47.735 },
 		{ -16.498, -135.180,  46.225 },
@@ -136,23 +136,23 @@ BOOST_AUTO_TEST_CASE(axis_pair_negation_y_and_z) {
 		{ -21.726, -135.373,  42.223 }
 	} };
 	const double RMSD_ACCURACY(0.00000001);
-	BOOST_CHECK_SMALL(calc_pairwise_superposition_rmsd(coord_list1, coord_list1_flipped_around_x), RMSD_ACCURACY );
-	BOOST_CHECK_SMALL(calc_pairwise_superposition_rmsd(coord_list1, coord_list1_flipped_around_y), RMSD_ACCURACY );
-	BOOST_CHECK_SMALL(calc_pairwise_superposition_rmsd(coord_list1, coord_list1_flipped_around_z), RMSD_ACCURACY );
+	BOOST_CHECK_SMALL(calc_pairwise_superposition_rmsd(coord_list_1, coord_list_1_flipped_around_x), RMSD_ACCURACY );
+	BOOST_CHECK_SMALL(calc_pairwise_superposition_rmsd(coord_list_1, coord_list_1_flipped_around_y), RMSD_ACCURACY );
+	BOOST_CHECK_SMALL(calc_pairwise_superposition_rmsd(coord_list_1, coord_list_1_flipped_around_z), RMSD_ACCURACY );
 }
 
 /// \brief TODOCUMENT
 BOOST_AUTO_TEST_CASE(rmsd) {
 	BOOST_CHECK_CLOSE(
 		rmsd_between_1_and_2,
-		calc_pairwise_superposition_rmsd(coord_list1, coord_list2),
+		calc_pairwise_superposition_rmsd(coord_list_1, coord_list_2),
 		ACCURACY_PERCENTAGE()
 	);
 }
 
 BOOST_AUTO_TEST_CASE(to_json_string_works_for_example_sup) {
 	BOOST_CHECK_EQUAL(
-		to_json_string( create_pairwise_superposition( coord_list1, coord_list2 ), false ),
+		to_json_string( create_pairwise_superposition( coord_list_1, coord_list_2 ), false ),
 		R"({"transformations":[{"translation":)"
 		R"({"x":"0","y":"0","z":"0"},)"
 		R"("rotation":)"
