42e2f38f86728b2f3761f3aa5f0cc0b526ec4e91
Tony Lewis <tonyelewis@users.noreply.github.com>
2015-08-17 08:33:24 +0100

    Remove explicit from coord_list's coord_vec ctor

--- source/structure/geometry/coord_list.h
+++ source/structure/geometry/coord_list.h
@@ -39,7 +39,7 @@ namespace cath {
 
 		public:
 			coord_list() = default;
-			explicit coord_list(const coord_vec &);
+			coord_list(const coord_vec &);
 
 			void reserve(const size_t &);
 			bool empty() const noexcept;
