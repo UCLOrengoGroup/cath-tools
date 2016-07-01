8bc1e244be15a2c6a6770c5b6af83426ee6865f5
Tony Lewis <tonyelewis@users.noreply.github.com>
2016-07-01 17:34:30 +0100

    Skip PDB records with occupancy other than 'A'/' '
    This is as discussed with Ian and Natalie

--- source/file/pdb/pdb_test.cpp
+++ source/file/pdb/pdb_test.cpp
@@ -150,4 +150,58 @@ ATOM   1875  CB  VAL B 140     -37.331  37.579 101.757  1.00 93.10           C)"
 	BOOST_REQUIRE_EQUAL( the_pdb.get_num_residues(), 2 );
 	BOOST_CHECK( icontains( test_ss.str(), "skip" ) );
 }
+
+
+BOOST_AUTO_TEST_CASE(skips_and_warns_on_alt_loc_other_than_space_or_a) {
+	// Example is from chain A of 3nir
+	const string input_string = R"(ATOM    421  N  APRO A  22      -3.223 -12.679   4.864  0.70  1.90           N  
+ATOM    422  CA APRO A  22      -2.719 -13.510   5.958  0.66  1.95           C  
+ATOM    423  C  APRO A  22      -1.267 -13.212   6.318  0.63  1.82           C  
+ATOM    424  O  APRO A  22      -0.409 -13.058   5.429  0.70  2.27           O  
+ATOM    425  CB APRO A  22      -2.869 -14.951   5.453  0.62  3.01           C  
+ATOM    426  CG APRO A  22      -4.087 -14.852   4.583  0.71  3.52           C  
+ATOM    427  CD APRO A  22      -3.965 -13.515   3.897  0.60  3.10           C  
+ATOM    428  HA APRO A  22      -3.484 -13.237   6.939  0.65  2.12           H  
+ATOM    429  HB2APRO A  22      -2.005 -15.242   4.842  0.65  3.12           H  
+ATOM    430  HB3APRO A  22      -3.029 -15.649   6.272  0.65  3.12           H  
+ATOM    431  HG2APRO A  22      -4.115 -15.658   3.853  0.65  3.84           H  
+ATOM    432  HG3APRO A  22      -4.996 -14.869   5.207  0.65  3.84           H  
+ATOM    433  HD2APRO A  22      -3.389 -13.631   2.975  0.65  3.36           H  
+ATOM    434  HD3APRO A  22      -4.941 -13.091   3.702  0.65  3.36           H  
+ATOM    435  N  BSER A1022      -3.018 -12.669   4.880  0.36  2.28           N  
+ATOM    436  CA BSER A1022      -2.587 -13.339   6.108  0.35  2.17           C  
+ATOM    437  C  BSER A1022      -1.146 -12.973   6.446  0.30  2.37           C  
+ATOM    438  O  BSER A1022      -0.307 -12.731   5.575  0.29  2.89           O  
+ATOM    439  CB BSER A1022      -2.704 -14.834   5.765  0.28  2.84           C  
+ATOM    440  OG BSER A1022      -1.766 -15.203   4.767  0.34  4.61           O  
+ATOM    441  H  BSER A1022      -3.534 -13.194   4.157  0.35  2.20           H  
+ATOM    442  HA BSER A1022      -3.031 -12.985   7.184  0.35  2.64           H  
+ATOM    443  HB2BSER A1022      -2.478 -15.439   6.624  0.35  3.56           H  
+ATOM    444  HB3BSER A1022      -3.676 -15.081   5.362  0.35  3.56           H  
+ATOM    445  HG BSER A1022      -1.905 -14.683   3.926  0.35  6.84           H  
+ATOM    446  N   GLU A  23      -0.939 -13.207   7.642  1.00  2.33           N  
+ATOM    447  CA  GLU A  23       0.439 -13.004   8.080  1.00  2.21           C  
+ATOM    448  C   GLU A  23       1.401 -13.972   7.382  1.00  2.08           C  
+ATOM    449  O   GLU A  23       2.520 -13.584   7.062  1.00  2.46           O  
+ATOM    450  CB  GLU A  23       0.597 -13.153   9.596  1.00  2.58           C  
+ATOM    451  CG  GLU A  23      -0.085 -12.041  10.394  1.00  3.18           C  
+ATOM    452  CD  GLU A  23       0.251 -12.089  11.902  1.00  4.41           C  
+ATOM    453  OE1 GLU A  23       0.630 -13.127  12.424  1.00  5.47           O  
+ATOM    454  OE2 GLU A  23       0.090 -11.025  12.579  1.00  6.76           O  
+ATOM    455  H   GLU A  23      -1.608 -13.536   8.317  1.00  2.47           H  
+ATOM    456  HA  GLU A  23       0.694 -12.123   7.844  1.00  2.32           H  
+ATOM    457  HB2 GLU A  23       0.152 -14.118   9.897  1.00  2.72           H  
+ATOM    458  HB3 GLU A  23       1.652 -13.179   9.840  1.00  2.72           H  
+ATOM    459  HG2 GLU A  23       0.253 -11.081  10.015  1.00  3.48           H  
+ATOM    460  HG3 GLU A  23      -1.163 -12.114  10.272  1.00  3.48           H  
+ATOM    461  HE2 GLU A  23       1.108 -11.273  13.151  1.00  9.48           H)";
+	ostringstream test_ss;
+	istringstream input_ss{ input_string };
+	const log_to_ostream_guard the_guard{ test_ss };
+
+	const pdb the_pdb = read_pdb_file( input_ss );
+
+	BOOST_REQUIRE_EQUAL( the_pdb.get_num_residues(), 2 );
+	BOOST_CHECK( icontains( test_ss.str(), "skip" ) );
+}
 BOOST_AUTO_TEST_SUITE_END()
