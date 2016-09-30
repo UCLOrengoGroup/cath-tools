f6de61da19560d7e3fe823dc924b3793251ab269
Tony Lewis <tonyelewis@users.noreply.github.com>
2016-09-30 16:37:57 +0100

    Add projected version of max_element / min_element

---
+++ source/common/boost_addenda/range/min_proj_element.h
@@ -0,0 +1,73 @@
+/// \file
+/// \brief The min_proj_element header
+
+/// \copyright
+/// Tony Lewis's Common C++ Library Code (here imported into the CATH Tools project and then tweaked, eg namespaced in cath)
+/// Copyright (C) 2007, Tony Lewis
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef MIN_PROJ_ELEMENT_H_INCLUDED
+#define MIN_PROJ_ELEMENT_H_INCLUDED
+
+#include <boost/range/algorithm/min_element.hpp>
+
+#include "common/boost_addenda/range/range_concept_type_aliases.h"
+#include "common/function/ident.h"
+
+namespace cath {
+	namespace common {
+
+		/// \brief Wrap boost::range::min_element() and add support for a projection function, a la Eric Niebler's range-v3 library
+		///
+		/// \todo Roll this out to simplify code
+		template <typename Rng,
+		          typename Pred = std::less<>,
+		          typename Proj = ident>
+		inline auto min_proj_element(Rng  &&arg_range, ///< The range to query
+		                             Pred &&arg_pred,  ///< The less-than predicate function
+		                             Proj &&arg_proj   ///< The projection fuction
+		                             ) {
+			/// \todo Come Clang (>= 3.7?) with fix, drop this type alias and use generic lambdas
+			using const_reference_type = common::range_const_reference_t<Rng>;
+
+			return boost::range::min_element(
+				arg_range,
+				[&] (const_reference_type x, const_reference_type y) {
+					return std::forward<Pred>( arg_pred )( arg_proj( x ), arg_proj( y ) );
+				}
+			);
+		}
+
+		/// \brief Return the projection of the min_proj_element() of the specified range
+		///
+		/// \todo Roll this out to simplify code
+		template <typename Rng,
+		          typename Pred = std::less<>,
+		          typename Proj = ident>
+		inline auto min_proj(Rng  &&arg_range, ///< The range to query
+		                     Pred &&arg_pred,  ///< The less-than predicate function
+		                     Proj &&arg_proj   ///< The projection fuction
+		                     ) {
+			return arg_proj( *min_proj_element(
+				std::forward< Rng  >( arg_range ),
+				std::forward< Pred >( arg_pred  ),
+				std::forward< Proj >( arg_proj  )
+			) );
+		}
+
+	}
+}
+
+#endif
---
+++ source/common/function/ident.h
@@ -0,0 +1,44 @@
+/// \file
+/// \brief The ident header
+
+/// \copyright
+/// Tony Lewis's Common C++ Library Code (here imported into the CATH Tools project and then tweaked, eg namespaced in cath)
+/// Copyright (C) 2007, Tony Lewis
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef IDENT_H_INCLUDED
+#define IDENT_H_INCLUDED
+
+namespace cath {
+	namespace common {
+
+		/// \brief Function object to return the argument
+		class ident final {
+		public:
+			/// \brief Return the argument using perfect-forwarding
+			template <typename T>
+			T && operator()(T &&arg_value
+			                ) const noexcept {
+				return std::forward<T>( arg_value );
+			}
+
+			/// \brief Indicate that this can be used in any smart, heterogeneous-lookup magic
+			using is_transparent = void;
+		};
+
+	}
+}
+
+#endif
