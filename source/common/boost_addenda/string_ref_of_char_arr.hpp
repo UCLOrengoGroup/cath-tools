4c0964c3dd266a789a1c2090898487cfbbba429f
Tony Lewis <tonyelewis@users.noreply.github.com>
2017-01-26 18:06:18 +0000

    Add fn to make string_ref from std::array<char, N>

---
+++ source/common/boost_addenda/string_ref_of_char_arr.hpp
@@ -0,0 +1,53 @@
+/// \file
+/// \brief The string_ref_of_char_arr header
+
+/// \copyright
+/// Tony Lewis's Common C++ Library Code (here imported into the CATH Tools project and then tweaked, eg namespaced in cath)
+/// Copyright (C) 2007, Tony Lewis
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _CATH_TOOLS_SOURCE_COMMON_BOOST_ADDENDA_STRING_REF_OF_CHAR_ARR_H
+#define _CATH_TOOLS_SOURCE_COMMON_BOOST_ADDENDA_STRING_REF_OF_CHAR_ARR_H
+
+#include <boost/utility/string_ref.hpp>
+
+#include "common/cpp14/cbegin_cend.hpp"
+
+#include <string>
+
+namespace cath {
+	namespace common {
+
+		/// \brief Make a string_ref from the specified array of chars
+		template <size_t N>
+		inline boost::string_ref string_ref_of_char_arr(const std::array<char, N> &arg_char_arr ///< The array of chars from which to make the string_ref
+		                                                ) {
+			return { common::cbegin( arg_char_arr ), N };
+		}
+
+		/// \brief Make a string from the specified array of chars
+		template <size_t N>
+		inline std::string string_of_char_arr(const std::array<char, N> &arg_char_arr ///< The array of chars from which to make the string
+		                                      ) {
+			return {
+				common::cbegin( arg_char_arr ),
+				common::cend  ( arg_char_arr )
+			};
+		}
+
+	} // namespace common
+} // namespace cath
+
+#endif
