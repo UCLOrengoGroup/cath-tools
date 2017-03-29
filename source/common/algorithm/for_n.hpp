e07fc12f124dcf6795a81943710fbf35df0bda39
Tony Lewis <tonyelewis@users.noreply.github.com>
2017-03-29 13:23:06 +0100

    Add and test simple for_n() function

---
+++ source/common/algorithm/for_n.hpp
@@ -0,0 +1,49 @@
+/// \file
+/// \brief The for_n() header
+
+/// \copyright
+/// Tony Lewis's Common C++ Library Code (here imported into the CATH Tools project and then tweaked, eg namespaced in cath)
+/// Copyright (C) 2007, Tony Lewis
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _CATH_TOOLS_SOURCE_COMMON_ALGORITHM_FOR_N_H
+#define _CATH_TOOLS_SOURCE_COMMON_ALGORITHM_FOR_N_H
+
+#include <boost/core/ignore_unused.hpp>
+#include <boost/range/irange.hpp>
+
+#include "common/cpp17/invoke.hpp"
+#include "common/size_t_literal.hpp"
+
+#include <cstddef>
+
+namespace cath {
+	namespace common {
+
+		/// \brief Invoke the specified callable the specified number of times
+		template <typename Fn>
+		void for_n(const size_t  &arg_n, ///< The number of time to invoke the callable
+		           Fn           &&arg_fn ///< The callable to invoke the specified number of times
+		           ) {
+			for (const auto &x : boost::irange( 0_z, arg_n ) ) {
+				boost::ignore_unused( x );
+				invoke( std::forward<Fn>( arg_fn ) );
+			}
+		}
+
+	} // namespace common
+} // namespace cath
+
+#endif
---
+++ source/common/algorithm/for_n_test.cpp
@@ -0,0 +1,35 @@
+/// \file
+/// \brief The for_n test suite
+
+/// \copyright
+/// Tony Lewis's Common C++ Library Code (here imported into the CATH Tools project and then tweaked, eg namespaced in cath)
+/// Copyright (C) 2007, Tony Lewis
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "for_n.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace cath::common;
+
+BOOST_AUTO_TEST_SUITE(for_n_test_suite)
+
+BOOST_AUTO_TEST_CASE(basic) {
+	int n = 10;
+	for_n( 5, [&] () { ++n;} );
+	BOOST_CHECK_EQUAL( n, 15 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
