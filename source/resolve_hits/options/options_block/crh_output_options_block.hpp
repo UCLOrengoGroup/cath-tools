1b887cb727c3c659aa5473b43ddfd8fe16170d34
Tony Lewis <tonyelewis@users.noreply.github.com>
2017-05-23 13:45:12 +0100

    Add crh_output_options_block and crh_output_spec
    These are to handle multiple outputs

---
+++ source/resolve_hits/options/options_block/crh_output_options_block.hpp
@@ -0,0 +1,69 @@
+/// \file
+/// \brief The crh_output_options_block class header
+
+/// \copyright
+/// CATH Tools - Protein structure comparison tools such as SSAP and SNAP
+/// Copyright (C) 2011, Orengo Group, University College London
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _CATH_TOOLS_SOURCE_RESOLVE_HITS_OPTIONS_OPTIONS_BLOCK_CRH_OUTPUT_OPTIONS_BLOCK_H
+#define _CATH_TOOLS_SOURCE_RESOLVE_HITS_OPTIONS_OPTIONS_BLOCK_CRH_OUTPUT_OPTIONS_BLOCK_H
+
+#include "options/options_block/options_block.hpp"
+#include "resolve_hits/options/options_block/crh_single_output_options_block.hpp"
+#include "resolve_hits/options/spec/crh_output_spec.hpp"
+
+namespace cath {
+	namespace rslv {
+
+		/// \brief Define an options_block for options specifying how cath-resolve-hits should write the output
+		class crh_output_options_block final : public opts::options_block {
+		private:
+			using super = opts::options_block;
+
+			// /// \brief The spec this options_block configures
+			crh_output_spec the_spec;
+
+			/// \brief The options_block that provides for the (soon-to-be) deprecated single-output options
+			///
+			/// \TODO Remove this after a reasonable time of it being deprecated
+			crh_single_output_options_block deprecated_single_output_ob;
+
+			std::unique_ptr<opts::options_block> do_clone() const final;
+			std::string do_get_block_name() const final;
+			void do_add_visible_options_to_description(boost::program_options::options_description &) final;
+			void do_add_hidden_options_to_description(boost::program_options::options_description &) final;
+			str_opt do_invalid_string(const boost::program_options::variables_map &) const final;
+			str_vec do_get_all_options_names() const final;
+
+		public:
+			static const std::string PO_HITS_TEXT_TO_FILE;
+			static const std::string PO_QUIET;
+			static const std::string PO_OUTPUT_TRIMMED_HITS;
+			static const std::string PO_SUMMARISE_TO_FILE;
+			static const std::string PO_HTML_OUTPUT_TO_FILE;
+			static const std::string PO_JSON_OUTPUT_TO_FILE;
+			static const std::string PO_EXPORT_CSS_FILE;
+			static const std::string PO_OUTPUT_HMMSEARCH_ALN;
+
+			const crh_output_spec & get_crh_output_spec() const;
+		};
+
+		crh_out_format get_out_format(const crh_output_options_block &);
+
+	} // namespace rslv
+} // namespace cath
+
+#endif
---
+++ source/resolve_hits/options/spec/crh_output_spec.cpp
@@ -0,0 +1,189 @@
+/// \file
+/// \brief The crh_output_spec class definitions
+
+/// \copyright
+/// CATH Tools - Protein structure comparison tools such as SSAP and SNAP
+/// Copyright (C) 2011, Orengo Group, University College London
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "crh_output_spec.hpp"
+
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/range/algorithm/count.hpp>
+
+#include "common/algorithm/append.hpp"
+#include "common/algorithm/is_uniq.hpp"
+#include "common/algorithm/sort_uniq_copy.hpp"
+#include "resolve_hits/options/options_block/crh_output_options_block.hpp"
+
+using namespace cath::common;
+using namespace cath::rslv;
+using namespace cath;
+using namespace std::literals::string_literals;
+
+using boost::filesystem::path;
+using boost::none;
+using boost::numeric_cast;
+using boost::range::count;
+using std::string;
+
+constexpr bool                crh_output_spec::DEFAULT_QUIET;
+constexpr hit_boundary_output crh_output_spec::DEFAULT_BOUNDARY_OUTPUT;
+constexpr bool                crh_output_spec::DEFAULT_OUTPUT_HMMSEARCH_ALN;
+
+/// \brief Getter for any files to which hits text should be output
+const path_vec & crh_output_spec::get_hits_text_files() const {
+	return hits_text_files;
+}
+
+/// \brief Getter for whether to suppress the default output of hits text to stdout
+const bool & crh_output_spec::get_quiet() const {
+	return quiet;
+}
+
+/// \brief Getter for whether to output the hits starts/stops *after* trimming
+const hit_boundary_output & crh_output_spec::get_boundary_output() const {
+	return boundary_output;
+}
+
+/// \brief Getter for any files to which a summary of the input should be output
+const path_vec & crh_output_spec::get_summarise_files() const {
+	return summarise_files;
+}
+
+/// \brief Getter for any files to which HTML should be output
+const path_vec & crh_output_spec::get_html_output_files() const {
+	return html_output_files;
+}
+
+/// \brief Getter for any files to which JSON should be output
+const path_vec & crh_output_spec::get_json_output_files() const {
+	return json_output_files;
+}
+
+/// \brief Getter for any files to which the HTML's CSS should be output
+const path_opt & crh_output_spec::get_export_css_file() const {
+	return export_css_file;
+}
+
+/// \brief Getter for whether to output a summary of the hmmsearch output alignment
+const bool & crh_output_spec::get_output_hmmsearch_aln() const {
+	return output_hmmsearch_aln;
+}
+
+/// \brief Setter for any files to which hits text should be output
+crh_output_spec & crh_output_spec::set_hits_text_files(const path_vec &arg_hits_text_files ///< Any files to which hits text should be output
+                                                       ) {
+	hits_text_files = arg_hits_text_files;
+	return *this;
+}
+
+/// \brief Setter for whether to suppress the default output of hits text to stdout
+crh_output_spec & crh_output_spec::set_quiet(const bool &arg_quiet ///< Whether to suppress the default output of hits text to stdout
+                                             ) {
+	quiet = arg_quiet;
+	return *this;
+}
+
+/// \brief Setter for whether to output the hits starts/stops *after* trimming
+crh_output_spec & crh_output_spec::set_boundary_output(const hit_boundary_output &arg_boundary_output ///< Whether to output the hits starts/stops *after* trimming
+                                                       ) {
+	boundary_output = arg_boundary_output;
+	return *this;
+}
+
+/// \brief Setter for any files to which a summary of the input should be output
+crh_output_spec & crh_output_spec::set_summarise_files(const path_vec &arg_summarise_files ///< Any files to which a summary of the input should be output
+                                                       ) {
+	summarise_files = arg_summarise_files;
+	return *this;
+}
+
+/// \brief Setter for any files to which HTML should be output
+crh_output_spec & crh_output_spec::set_html_output_files(const path_vec &arg_html_output_files ///< Any files to which HTML should be output
+                                                         ) {
+	html_output_files = arg_html_output_files;
+	return *this;
+}
+
+/// \brief Setter for any files to which JSON should be output
+crh_output_spec & crh_output_spec::set_json_output_files(const path_vec &arg_json_output_files ///< Any files to which JSON should be output
+                                                         ) {
+	json_output_files = arg_json_output_files;
+	return *this;
+}
+
+/// \brief Setter for any files to which the HTML's CSS should be output
+crh_output_spec & crh_output_spec::set_export_css_file(const path_opt &arg_export_css_file ///< Any files to which the HTML's CSS should be output
+                                                       ) {
+	export_css_file = arg_export_css_file;
+	return *this;
+}
+
+/// \brief Setter for whether to output a summary of the hmmsearch output alignment
+crh_output_spec & crh_output_spec::set_output_hmmsearch_aln(const bool &arg_output_hmmsearch_aln ///< Whether to output a summary of the hmmsearch output alignment
+                                                            ) {
+	output_hmmsearch_aln = arg_output_hmmsearch_aln;
+	return *this;
+}
+
+/// \brief Get all output paths implied by the specified crh_output_spec
+///
+/// \relates crh_output_spec
+path_vec cath::rslv::get_all_output_paths(const crh_output_spec &arg_output_spec ///< The crh_output_spec to query
+                                          ) {
+	path_vec the_paths;
+	append( the_paths, arg_output_spec.get_hits_text_files()   );
+	append( the_paths, arg_output_spec.get_summarise_files()   );
+	append( the_paths, arg_output_spec.get_html_output_files() );
+	append( the_paths, arg_output_spec.get_json_output_files() );
+	if ( arg_output_spec.get_export_css_file() ) {
+		the_paths.push_back( *arg_output_spec.get_export_css_file() );
+	}
+	return the_paths;
+}
+
+/// \brief Generate a description of any problem that makes the specified crh_output_spec invalid
+///        or none otherwise
+///
+/// \relates crh_output_spec
+str_opt cath::rslv::get_invalid_description(const crh_output_spec &arg_output_spec ///< The crh_output_spec to query
+                                            ) {
+	const auto all_sorted_paths = sort_copy( get_all_output_paths( arg_output_spec ) );
+
+	const size_t num_stdouts = numeric_cast<size_t>( count( all_sorted_paths, path{ "-" } ) );
+	if ( num_stdouts > 1 ) {
+		return "Cannot send more than one type of output to stdout (which is specified as file \"-\")"s;
+	}
+
+	if ( num_stdouts > 0 && arg_output_spec.get_quiet() ) {
+		return "Cannot send output to stdout when requesting --" + crh_output_options_block::PO_QUIET;
+	}
+
+	if ( ! is_uniq( all_sorted_paths ) ) {
+		return "Cannot send more than one type of output to the same output file"s;
+	}
+
+	return none;
+}
+
+/// \brief Convenience setter for whether to output the hits starts/stops *after* trimming
+///
+/// \relates crh_output_spec
+crh_output_spec & cath::rslv::set_output_trimmed_hits(crh_output_spec &arg_output_spec,        ///< The crh_output_spec to modify
+                                                      const bool      &arg_output_trimmed_hits ///< Whether to output the hits starts/stops *after* trimming
+                                                      ) {
+	return arg_output_spec.set_boundary_output( hit_boundary_output_of_output_trimmed_hits( arg_output_trimmed_hits ) );
+}
