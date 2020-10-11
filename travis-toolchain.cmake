049f18eec5ccdd5fae86a950a6d271629c33c6c4
Tony Lewis <tonyelewis@users.noreply.github.com>
2020-10-11 17:19:38 +0100

    Mv compiler warning settings to the toolchain file
    Also disable null-dereference for now.

--- travis-toolchain.cmake
+++ travis-toolchain.cmake
@@ -10,3 +10,25 @@
 #
 # So the following line disables the config file mode and reverts behaviour to as before.
 set( Boost_NO_BOOST_CMAKE 1 )
+
+add_compile_options(
+	-W
+	-Wall
+	-Werror
+	-Wextra
+	-Wcast-qual
+	-Wconversion
+	-Wformat=2
+	-Wnon-virtual-dtor
+	-Wold-style-cast
+	-Wshadow
+	-Wsign-compare
+	-Wsign-conversion
+	-pedantic
+	-ftabstop=2
+	$<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,4.8.0>>:-Wuseless-cast>
+	$<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,6.0.0>>:-Wduplicated-cond>
+	$<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,7.0.0>>:-Wduplicated-branches>
+	$<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,7.0.0>>:-Wrestrict>
+	$<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,8.0.0>>:-Wno-maybe-uninitialized>
+)
