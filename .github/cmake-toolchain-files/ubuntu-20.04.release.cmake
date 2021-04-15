03dab5ec1b2ae344c851eec3e39341513d5b3c57
Tony Lewis <tonyelewis@users.noreply.github.com>
2021-04-15 11:08:52 +0100

    Develop GH action

---
+++ .github/cmake-toolchain-files/ubuntu-20.04.release.cmake
@@ -0,0 +1,4 @@
+set( CMAKE_BUILD_TYPE Release )
+
+set( CMAKE_C_COMPILER   "/usr/bin/gcc-10" CACHE FILEPATH "The C compiler"   )
+set( CMAKE_CXX_COMPILER "/usr/bin/g++-10" CACHE FILEPATH "The C++ compiler" )
